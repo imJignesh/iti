03aff3a55cc8f80b60e59f4fd1e846ab2c33f697
AnkurSMB <ankur@smbsolutionkwt.com>
2025-06-30 13:40:34 +0530

    responsive

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+      />
+      <meta name="theme-color" content="#000000" />
+
       <body>
         <Main />
         <NextScript />
