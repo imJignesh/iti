fb5926283572305100172d17cda8eddb050335b5
Jesmin Sheladiya <119096904+Jesmin-1832@users.noreply.github.com>
2025-07-16 15:11:59 +0530

    Add files via upload

---
+++ src/pages/_app.js
@@ -0,0 +1,7 @@
+import "@/styles/globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/fonts/stylesheet.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Header from './Header';
+import Home from '../components/Home';
+import Footer from '@/pages/Footer';
+
+export default function Index() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
+}
